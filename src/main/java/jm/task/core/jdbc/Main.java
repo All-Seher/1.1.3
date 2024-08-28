cd508a71b41c9c17b6053df8d1aa4533923a3393
All_Seher <all.seherin@yandex.ru>
2024-08-29 01:18:36 +0500

    ITMPJ-2660: implemented all function

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,33 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.dropUsersTable();                                           //проверка на отсутствие исключения, при удалении несуществующей таблицы
+        userService.createUsersTable();
+        userService.createUsersTable();                                         //проверка на отсутствие исключения, при создании уже существующей таблицы
+
+        userService.saveUser("Ivan", "Ivanov", (byte) 20);
+        userService.saveUser("Petr", "Petrov", (byte) 21);
+        userService.saveUser("Sergey", "Sergeev", (byte) 22);
+        userService.saveUser("Vasiliy", "Vasilev", (byte) 23);
+
+        userService.removeUserById(1);                                          //удаление пользователя по id
+
+        List<User> users = userService.getAllUsers();
+        users.forEach(System.out::println);
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+        Util.closeConnection();                                                 //закрытие соединения с БД
     }
 }
