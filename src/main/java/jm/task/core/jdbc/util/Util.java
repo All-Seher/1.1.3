074e15ad9b5d26df5403db8d2e715df7897dffc3
All_Seher <all.seherin@yandex.ru>
2024-08-28 22:58:11 +0500

    ITMPJ-2660: Added connection with my own DB

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,41 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+    private static Util INSTANCE;
+    Connection connection;
+
+    private Util() {
+        String URL = "jdbc:postgresql://localhost:5432/postgres";
+        String USER = "postgres";
+        String PASSWORD = "1";
+
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Возвращает единственный экземпляр класса Connection
     // реализуйте настройку соеденения с БД
+    public static Connection getConnection() {
+        if(INSTANCE == null) {
+            INSTANCE = new Util();
+        }
+
+        return INSTANCE.connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            INSTANCE.connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
